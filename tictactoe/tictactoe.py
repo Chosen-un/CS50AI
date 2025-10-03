eb5c1157bea9fe04ba07237fc020b74912ef7721
Prince <princeisno.01@gmail.com>
2025-10-03 13:27:53 +0100

    first commit

---
+++ tictactoe/tictactoe.py
@@ -0,0 +1,160 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+import copy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    x_count = sum(row.count(X) for row in board)
+    o_count = sum(row.count(O) for row in board)
+
+    if x_count > o_count:
+        return O
+    else:
+        return X
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    possible_actions = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i, j))
+    return possible_actions
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    i, j = action
+    if i < 0 or i > 2 or j < 0 or j > 2 or board[i][j] is not EMPTY:
+        raise Exception("Invalid action")
+
+    new_board = copy.deepcopy(board)
+    new_board[i][j] = player(board)
+    return new_board
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    # Check rows
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] is not EMPTY:
+            return board[i][0]
+
+    # Check columns
+    for j in range(3):
+        if board[0][j] == board[1][j] == board[2][j] and board[0][j] is not EMPTY:
+            return board[0][j]
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not EMPTY:
+        return board[0][2]
+
+    return None
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    if winner(board) is not None or all(cell is not EMPTY for row in board for cell in row):
+        return True
+    return False
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
+
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    if terminal(board):
+        return None
+
+    current_player = player(board)
+
+    if current_player == X:
+        # Maximizer wants to find the action with the highest value
+        best_value = -math.inf
+        best_move = None
+        for action in actions(board):
+            # min_value is called because it will be O's turn next
+            value = min_value(result(board, action))
+            if value > best_value:
+                best_value = value
+                best_move = action
+        return best_move
+    else:  # current_player == O
+        # Minimizer wants to find the action with the lowest value
+        best_value = math.inf
+        best_move = None
+        for action in actions(board):
+            # max_value is called because it will be X's turn next
+            value = max_value(result(board, action))
+            if value < best_value:
+                best_value = value
+                best_move = action
+        return best_move
+
+def max_value(board):
+    """
+    Returns the maximum utility value achievable from the current board state.
+    """
+    if terminal(board):
+        return utility(board)
+    
+    v = -math.inf
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+    return v
+
+
+def min_value(board):
+    """
+    Returns the minimum utility value achievable from the current board state.
+    """
+    if terminal(board):
+        return utility(board)
+
+    v = math.inf
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+    return v
